ec2f139def20253ab35e92575cd72373481ee661
Salim KASSIM <salim.kassim2024@campus-eni.fr>
2024-02-28 16:07:51 +0100

    Ajout du formulaire et du fichier css

---
+++ script/inscrire.js
@@ -0,0 +1,80 @@
+$(function(){
+
+
+
+});
+
+$("#inscription ").validate({ 
+    rules: {
+       nom:{
+          required: true,
+          minlength: 3,
+          maxlength: 255
+       },
+       email: {
+          required: true,
+          maxlength: 255,
+          email: true
+       },
+       mdp: {
+          required: true,
+          minlength: 6
+       },
+       mdpConfirme: {
+        required: true,
+        minlength: 6,
+        equalTo: "#mdp"
+        }
+    },
+    // message erreur
+    messages: {
+        nom:  {
+            required: "Choissisez un pseudo  contenant au moins 3 caractères."
+        },
+      email: "Rentrez un email valide",
+        mdp: "Au moins un symbole, un chiffre, ainsi que 6 carateres minimum.",
+      mdpConfirme: {
+        required : 'Veuillez confirmez le mot de passe',
+        equalTo : 'les mot de passe doivent être identique',
+    }
+
+  },
+   // bouton valider 
+    submitHandler: function(form) {
+       
+       var id = generateId();
+       var nom = $("#nom").val();
+       var email = $("#email").val();
+       var mdp = $("#mdp").val();
+       var mdpConfirm = $("#mdp").val();
+
+       login(id,nom,email,mdp,mdpConfirm);
+    }
+
+});
+
+// genere un identifiant unique
+function generateId(){
+      return  Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);;
+}
+
+
+function login(id,nom,email,mdp,mdpConfirm){
+// Test de la récuperation de mes données utilisateur(saisie)
+    console.log(id);
+    console.log(nom);
+    console.log(email);
+    console.log(mdp);
+    console.log(mdpConfirm);
+    // declaration de mon json
+    var utilisateur = [];
+    // ajout d'un utilisateur dans le json
+    utilisateur.push({'id': id, 'nom': nom,'email': email, 'mdp':mdp,'mdpConfirm':mdpConfirm});
+
+    var utilisateur_json = JSON.stringify(utilisateur);
+    // enregistrement de mon utilisateur dans ma session storage
+    sessionStorage.setItem("utilisateur",utilisateur_json);
+
+    console.log(JSON.stringify(utilisateur_json));
+}
+
