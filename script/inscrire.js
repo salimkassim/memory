0608f6051f1e80020e4bc7d9e656892c514a01f1
Salim KASSIM <salim.kassim2024@campus-eni.fr>
2024-02-29 09:44:52 +0100

    Modification du fichier inscrire.js pour l'enregistrement

--- script/inscrire.js
+++ script/inscrire.js
@@ -1,80 +1,86 @@
-$(function(){
+$(function () {});
 
-
-
-});
-
-$("#inscription ").validate({ 
-    rules: {
-       nom:{
-          required: true,
-          minlength: 3,
-          maxlength: 255
-       },
-       email: {
-          required: true,
-          maxlength: 255,
-          email: true
-       },
-       mdp: {
-          required: true,
-          minlength: 6
-       },
-       mdpConfirme: {
-        required: true,
-        minlength: 6,
-        equalTo: "#mdp"
-        }
+$("#inscription ").validate({
+  rules: {
+    nom: {
+      required: true,
+      minlength: 3,
+      maxlength: 255,
     },
-    // message erreur
-    messages: {
-        nom:  {
-            required: "Choissisez un pseudo  contenant au moins 3 caractères."
-        },
-      email: "Rentrez un email valide",
-        mdp: "Au moins un symbole, un chiffre, ainsi que 6 carateres minimum.",
-      mdpConfirme: {
-        required : 'Veuillez confirmez le mot de passe',
-        equalTo : 'les mot de passe doivent être identique',
-    }
-
+    email: {
+      required: true,
+      maxlength: 255,
+      email: true,
+    },
+    mdp: {
+      required: true,
+      minlength: 6,
+    },
+    mdpConfirme: {
+      required: true,
+      minlength: 6,
+      equalTo: "#mdp",
+    },
   },
-   // bouton valider 
-    submitHandler: function(form) {
-       
-       var id = generateId();
-       var nom = $("#nom").val();
-       var email = $("#email").val();
-       var mdp = $("#mdp").val();
-       var mdpConfirm = $("#mdp").val();
+  // message erreur
+  messages: {
+    nom: {
+      required: "Choissisez un pseudo  contenant au moins 3 caractères.",
+    },
+    email: "Rentrez un email valide",
+    mdp: "Au moins un symbole, un chiffre, ainsi que 6 carateres minimum.",
+    mdpConfirme: {
+      required: "Veuillez confirmez le mot de passe",
+      equalTo: "les mot de passe doivent être identique",
+    },
+  },
+  // bouton valider
+  submitHandler: function (form) {
+    var id = generateId();
+    var nom = $("#nom").val();
+    var email = $("#email").val();
+    var mdp = $("#mdp").val();
+    var mdpConfirm = $("#mdp").val();
 
-       login(id,nom,email,mdp,mdpConfirm);
-    }
-
+    inscription(id, nom, email, mdp, mdpConfirm);
+  },
 });
 
 // genere un identifiant unique
-function generateId(){
-      return  Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);;
+function generateId() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
 }
 
+function inscription(id, nom, email, mdp, mdpConfirm) {
+  // Test de la récuperation de mes données utilisateur(saisie)
+  console.log(id);
+  console.log(nom);
+  console.log(email);
+  console.log(mdp);
+  console.log(mdpConfirm);
 
-function login(id,nom,email,mdp,mdpConfirm){
-// Test de la récuperation de mes données utilisateur(saisie)
-    console.log(id);
-    console.log(nom);
-    console.log(email);
-    console.log(mdp);
-    console.log(mdpConfirm);
-    // declaration de mon json
-    var utilisateur = [];
-    // ajout d'un utilisateur dans le json
-    utilisateur.push({'id': id, 'nom': nom,'email': email, 'mdp':mdp,'mdpConfirm':mdpConfirm});
+  //debugger;
 
-    var utilisateur_json = JSON.stringify(utilisateur);
-    // enregistrement de mon utilisateur dans ma session storage
-    sessionStorage.setItem("utilisateur",utilisateur_json);
+  var utilisateurJson = sessionStorage.getItem("utilisateurEnregistrer");
 
-    console.log(JSON.stringify(utilisateur_json));
+  if (!utilisateurJson) {
+    utilisateurJson = [];
+  } else {
+    utilisateurJson = JSON.parse(utilisateurJson);
+  }
+
+  utilisateurJson.push({
+    id: id,
+    nom: nom,
+    email: email,
+    mdp: mdp,
+    mdpConfirm: mdpConfirm,
+  });
+
+  sessionStorage.setItem(
+    "utilisateurEnregistrer",
+    JSON.stringify(utilisateurJson)
+  );
 }
-
